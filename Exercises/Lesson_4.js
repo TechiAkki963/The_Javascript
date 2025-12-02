58a7cc37e0b3da3c596e91da8207fae3821ba7da
Akshay <paraba777@gmail.com>
2025-12-02 15:09:02 +0530

    variable exercises

---
+++ Exercises/Lesson_4.js
@@ -0,0 +1,16 @@
+const name = "Akshay";
+console.log(`My name is ${name}`);
+
+const coffeePrice = 5;
+const bagglePrice = 3;
+const soupPrice = 9;
+
+const Cost = coffeePrice + 2 * bagglePrice + soupPrice;
+console.log(`Cost: $${Cost}`);
+
+const taxRate = 0.1;
+console.log(`Tax Rate: ${taxRate}%`);
+
+console.log(`Total Cost: $${Cost + Cost * taxRate}`);
+
+let cartQuantity = 0;
