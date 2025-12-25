61e5f88321ddd35e4520919996a0c010a67f1b4b
Akshay <paraba777@gmail.com>
2025-12-26 01:20:52 +0530

    TODO basic

--- Code/scripts/11_todo_list.js
+++ Code/scripts/11_todo_list.js
@@ -1,5 +1,20 @@
 const todoList = [];
 
+task();
+
+function task() {
+  let taskListHTML = " ";
+
+  for (i = 0; i < todoList.length; i++) {
+    const task = todoList[i];
+    const html = `<p>${task}</p>`;
+
+    taskListHTML += html;
+  }
+  console.log(taskListHTML);
+
+  document.querySelector(".js-task-item").innerHTML = taskListHTML;
+}
 function addTodo() {
   const inputElement = document.querySelector(".js-name-input");
   const name = inputElement.value;
@@ -9,4 +24,6 @@ function addTodo() {
   console.log(todoList);
 
   inputElement.value = " ";
+
+  task();
 }
